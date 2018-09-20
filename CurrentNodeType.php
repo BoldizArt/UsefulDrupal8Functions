439ac5d9947dbf2ae6406598d5b8fff99d136452
Boldižar Santo <boldizar.santo@gmail.com>
2018-09-20 11:16:41 +0200

    Load the current node and check its type in Drupal 8.

---
+++ CurrentNodeType.php
@@ -0,0 +1,14 @@
+<?php
+// Load the current node.
+$node = \Drupal::request()->attributes->get('node');
+
+// Check if $node is object
+if (is_object($node)) {
+    // Get current node type
+    $typeName = $node->bundle();
+
+    if ($typeName == 'article') {
+        $title = $node->getTitle();
+        // ...
+    }
+}
--- README.txt
+++ README.txt
@@ -4,4 +4,6 @@ USEFUL DRUPAL8 FUNCTIONS
 Get current path, route name and path alias by current path:
     * CurrentPathRouteNameAndPathAlias.php
 
+Load the current node and check its type:
+    * CurrentNodeType.php
 
